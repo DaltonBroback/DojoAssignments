7a54019fa29ad485384518d3756f30a31b64e1eb
DaltonBroback <dbroback@gmail.com>
2017-05-11 15:31:45 -0700

    Re-added DisplayDate to when the app starts up

--- iOS/iOS/Basics/DateTimePlayground.playground/Contents.swift
+++ iOS/iOS/Basics/DateTimePlayground.playground/Contents.swift
@@ -2,9 +2,5 @@
 
 import UIKit
 
-let date = Date()
-let calendar = Calendar.current
-let hour = calendar.component(.hour, from: date)
-let minutes = calendar.component(.minute, from: date)
 
-print(date)
+
